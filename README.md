037b81113fc23f9fefceb4004e325709a165bd19
huangzhen <huangzhen@gyyx.cn>
2016-06-21 21:05:11 +0800

    init

---
+++ README.md
@@ -0,0 +1,304 @@
+# 1	概述 #
+一套跨平台远程管理框架，支持用户根据自身的业务进行多语言应用开发
+
+# 2	实现目标和意义 #
+1. 作为系统部对机器进行远程管理的中间件
+1. 实现运维人员可以在从单点触发管理目前线上的机器（Windows/Linux）
+1. 使开发人员可以方便的接入远程管理系统，并通过远程管理接口操作线上机器，实现运维自动化
+1. 实现机器的定时任务集中管理
+
+**1	通讯协议、在线检测与配置文件**
+通讯协议基于Thrift，并且Scheduler与Agent全部注册到Zookeeper上，确保客户端的时效性，并且存储部分配置文件信息
+
+**2	管理界面与外部接口**
+提供一套统一的接口，允许系统调用接口实现远程内部所有功能。并且基于统一接口为用户提供一套管理界面，允许用户在管理界面上进行功能使用。
+
+**3	操作模式**
+所有用户/系统操作均由Scheduler端进行中转，并且中心端记录所有详细的操作日志
+
+**4	应用与语言**
+支持业务应用的开发，并且支持多语言支持(Python,Java,C#)，应用支持动态加载，可以在不重启Scheduler与Agent时执行相应业务应用
+
+# 3	架构与组件 #
+
+### 3.1	组件与部署结构 ###
+![](designer/snapshot/struct.jpg)
+
+**Elves-Scheduler**
+Elves中心调度器,整套系统的核心，连接Apps与Agents的媒介，通过数据库获取异步任务或通过接口获取同步任务，进行任务中转与分发工作。
+
+**Elves-Agent**
+Elves任务代理器，部署在每台被控机上，主要接受来自Scheduler的指令，下载并加载相应的App-Worker，执行后将相应的执行结果发送至Scheduler端，Agent以Deamon形式运行，同时提供本身的定时自调用。
+
+**App**
+Elves业务应用，Elves系统本身不提供任何业务功能，仅作为任务转发的媒介，App为加载到Elves上的业务应用，每个App对应一个或多个指令，Elves支持不限量的Apps业务，所以如果业务需要，只需要单独开发App并加载到Elves即可。
+
+App结构图
+
+![](designer/snapshot/module-struct.jpg)
+
+>逻辑组件:Worker : 在Agent端执行的业务逻辑组件，此组件自动加载到Agent端，并在被控机器上执行相应业务逻辑
+
+>逻辑组件:Processor : 在App端执行的业务处理逻辑组件，此组件接收来自Agent端(Worker)执行后返回的结果，并作后续处理
+
+
+**开放平台:OpenAPI**
+Elves开放平台，提供RESTful形式的API供第三方系统开发调用，并同时提供OpenApi的后台管理功能
+
+
+# 4	概念与分层 #
+
+![](designer/snapshot/ceng.jpg)
+
+## 4.1	通讯模式 ##
+*指令的工作模式*
+
+**异步回调(Async)**
+Agent在收到指令后异步执行相应的指令内容，执行结束后通过回调方式将内容反馈至Scheduler,Agent需主动连接Scheduler
+
+**同步调用(Sync)**
+Agent在收到指令后同步执行相应的指令内容，执行结束后将直接反馈内容至Scheduler,Agent无需主动连接Scheduler
+
+## 4.2 发起方式 ##
+*指令的发起方式*
+
+**Agent发起(Agent)**
+由Agent端自发的在本地进行任务的发起
+
+**Scheduler发起(Scheduler)**
+由Scheduler端发起任务触发Agent执行任务
+
+## 4.2	处理类型 ##
+*指令的一次完整生命周期的类型*
+
+**非处理型(NoProcessor)**
+该指令在Agent上运行后执行结束
+
+**处理型(Processor)**
+该指令在Agent上运行后将执行数据通过Scheduer回调至App后执行结束
+
+## 4.3 工作模式 ##
+
+>**AAnP** | 工作模式：异步模式 | 发起方式：Agent发起 | 生命类型：非处理型
+
+![](designer/snapshot/workmode/AAnP.png)
+
+>**AAP** | 工作模式：异步模式  | 发起方式：Agent发起 | 生命类型：处理型
+
+![](designer/snapshot/workmode/AAP.png)
+
+>**SAnP** | 工作模式：异步模式  | 发起方式：Scheduler发起 |  生命类型：非处理型
+
+![](designer/snapshot/workmode/SAnP.png)
+
+>**SAP** | 工作模式：异步模式 |  发起方式：Scheduler发起 |  生命类型：处理型
+
+![](designer/snapshot/workmode/SAP.png)
+
+>**SSnP** | 工作模式:同步模式  |  发起方式:Scheduler发起   |  生命类型:非处理型
+
+![](designer/snapshot/workmode/SSnP.png)
+
+>**SSP** | 工作模式:同步模式  |  发起方式:Scheduler发起   |  生命类型:处理型
+
+![](designer/snapshot/workmode/SSP.png)
+
+## 4.3	任务 ##
+
+*目前Elves支持以下3种任务*
+
+**计划任务(Cron)**
+根据规则周期性的执行某项任务
+
+>支持模式：AAnP，AAP，SAnP，SAP
+
+基础自主计划任务通过Agent存储于本地文件系统中的任务执行规则 或 通过Scheduler的任务执行规则由Scheduler进行任务指令的发送，由Agent自发的在本地进行任务的运行后结束 或 由Agent自发的在本地进行任务的运行后通过Scheduler回调至App-Processor后结束
+
+
+**队列任务(Queue)**
+异步并发方式地执行用户提交的某项任务
+	
+>支持模式：SAnP，SAP
+
+中心计划任务通过Scheduler的任务执行规则由Scheduler进行任务指令的发送，Agent在接收到指令后进行任务的运行后结束 或 Agent在接收到指令后进行任务的运行后通过Scheduler回调至App-Processor后结束
+
+注意：队列任务可以设置依赖关系，但当且仅当使用SAP模式才可使用其依赖功能
+
+
+**即时任务(RT)**
+即时阻塞方式执行用户提交的某项任务
+
+>支持模式：SSP
+
+及时任务为用户/第三方系统触发，并以同步调用方式在被控机上执行相应任务，实时获取相应执行结果后结束
+
+
+# 5	系统设计 #
+## 5.1	重点时序 ##
+
+
+## 5.2	通讯结构体  ##
+
+	//命令构体
+	struct Instruct{
+	    1 : string id,		//指令ID
+	    2 : string app,		//APP
+	    3 : string func,	//方法
+	    4 : string param	//参数
+	    5 : i32 timeout		//超时时间（默认不设置则无超时时间）
+	    6 : string proxy	//代理器（默认win系统使用agentProxy.exe,linx使用agentProxy.py）
+	}
+	
+	//命令结果结构体
+	struct Reinstruct{
+	    1 : string id,      //指令ID       
+	    2 : i32 flag,		//状态
+	    3 : i32 costtime,	//耗时
+	    4 : string result	//结果
+	}
+
+## 5.3	代码结构 ##
+
+    .
+	├─3rd-lib			#elves依赖的第三方包/类库
+	├─designer			#elves设计相关附件
+    ├─elves				#elves主代码,scheduler代码
+    ├─elves-agent		#elves Agent组件
+    ├─elves-openapi		#elves OpenApi站点
+    └─elves-appsdk		    #elves app组件(示例)
+
+## 5.4 ZooKeeper ##
+
+    /								Zookeeper根节点
+    └─Elves							Manager根节点(可自定义名称)
+      └─Scheduler							scheduler根节点，存储记录scheduler自增ID号
+          └─IP,IP,IP							Scheduler的IP信息，存储记录此scheduler是否正常
+      └─Agent								Agent根节点
+          └─IP,IP,IP							Agent的IP，存储记录该Agent的计划任务与已加载App
+      └─App							App根节点
+          └─GYMonitor						    App的名称,内容存储版本号
+			└─IP,IP,IP								数据存储，当前App的端口号
+
+**全局配置文件存储与Elves根节点**
+
+	{"SchedulerPort": "11000","Timeout": "80000","AgentPort": "11101","HttpRes": "http://192.168.6.116", "HttpResUSer": "admin", "HttpResPass": "1q2w3e4r"}
+
+1. SchedulerPort ： Scheduler与Agent/App的交互端口，默认用户接口端口在此基础上加1
+1. Timeout ： Scheduler与Agent/App的交互时的超时时间，只针对异步模式
+1. AgentPort ： Agent对外提供服务的端口号
+1. HttpRes ： HTTP资源下载地址，用户Agent端更新，Worker组件下载
+1. HttpResUSer ： HTTP资源下载的账号
+1. HttpResPass : HTTP资源下载的密码
+
+## 5.5	核心库物理模型 ##
+![](designer/snapshot/pdm.jpg)
+
+## 5.6	组件设计 ##
+### 5.6.1 调度器-scheduler ###
+**详见：** [elves-scheduler 详细设计](elves)
+
+### 5.6.2 代理器-Agent ###
+**详见：** [elves-agent 详细设计](elves-agent)
+
+### 5.6.3 应用-App ###
+**详见：** [elves-app 详细设计](elves-appsdk)
+
+### 5.6.4 开放平台-OpenAPI ###
+**详见：** [elves-openapi 详细设计](elves-openapi)
+
+# 6 高级功能 #
+
+高级功能为Elves内置的Base App提供的功能
+
+## 6.1 定时器Cron ##
+
+定时器功能支持Scheduler端发起或Agent端发起
+
+规则说明
+
+ 	序号	说明	 是否必填	 允许填写的值	允许的通配符
+ 	1	 秒	 是	 0-59 	  , - * /
+ 	2	 分	 是	 0-59	  , - * /
+ 	3	小时	 是	 0-23	  , - * /
+ 	4	 日	 是	 1-31	  , - * ? / L W
+ 	5	 月	 是	 1-12 or JAN-DEC	  , - * /
+ 	6	 周	 是	 1-7 or SUN-SAT	  , - * ? / L #
+ 	7	 年	 否	 empty 或 1970-2099	 , - * /
+
+**通配符说明:**
+
+\* 表示所有值. 例如:在分的字段上设置 "*",表示每一分钟都会触发。
+
+
+? 表示不指定值。使用的场景为不需要关心当前设置这个字段的值。例如:要在每月的10号触发一个操作，但不关心是周几，所以需要周位置的那个字段设置 为"?" 具体设置为 0 0 0 10 * ?
+
+\- 表示区间。例如 在小时上设置 "10-12",表示 10,11,12点都会触发。
+ 
+, 表示指定多个值，例如在周字段上设置 "MON,WED,FRI" 表示周一，周三和周五触发
+
+/ 用于递增触发。如在秒上面设置"5/15" 表示从5秒开始，每增15秒触发(5,20,35,50)。 在月字段上设置'1/3'所示每月1号开始，每隔三天触发一次。
+
+L 表示最后的意思。在日字段设置上，表示当月的最后一天(依据当前月份，如果是二月还会依据是否是润年[leap]), 在周字段上表示星期六，相当于"7"或"SAT"。如果在"L"前加上数字，则表示该数据的最后一个。例如在周字段上设置"6L"这样的格式,则表示“本 月最后一个星期五"
+
+W 表示离指定日期的最近那个工作日(周一至周五). 例如在日字段上设置"15W"，表示离每月15号最近的那个工作日触发。如果15号正好是周六，则找最近的周五(14号)触发, 如果15号是周未，则找最近的下周一(16号)触发.如果15号正好在工作日(周一至周五)，则就在该天触发。如果指定格式为 "1W",它则表示每月1号往后最近的工作日触发。如果1号正是周六，则将在3号下周一触发。(注，"W"前只能设置具体的数字,不允许区间"-").
+
+
+**Agent Func : Cron**
+
+用于编辑Agent端发起的Cron任务
+
+Param
+
+	method	 : add/del
+	id       : xxxxxxx
+	app      : os_uniform
+	func     : check_pc
+    param    : {"aaa":"ccc"}
+    rule     : * * * * * *
+
+字段解释
+
+	method 操作方法	add:新增,del:删除
+	id     计划任务ID
+	app    应用
+	func   方法
+	param  参数
+	rule   规则
+
+
+## 6.2 自更新AgentUpdate ##
+
+**Func : AgentUpdate**
+
+用于发起Agent更新任务
+
+Param
+
+	Version 		: v1.0.0
+	ResourceUrl     : http://elves.gyyx.cn/csharpv1.0.0.zip
+	
+字段解释
+
+	Version         Agent要更新的版本
+	ResourceUrl		Agent的更新文件地址
+	
+
+
+# 7 执行路线 #
+
+- 完成目录（包）结构制定，并完成核心的类结构制定
+- 完成队列任务的逻辑模块
+- 完成正式环境部署与社区业务机器(windows)应用
+- 完成定时任务
+- 完成Linux业务机器应用
+- 完成同步调用
+
+
+# 8 更新记录 #
+1. 20160519 更新代码结构将最外层的quartz-spring_demo移动至3rd-lib下
+2. 20160519 更新业务执行逻辑时序图，[5.1.2]，[5.1.3]，增加[5.1]章节程序架构
+2. 20160523 更新接口权限设计[5.4.2.2],[5.4.2.4]，增加业务逻辑组件章节[5.1.1.3.x]
+3. 20160531 修订Scheduler的命名，修订[5.5.1	目录树]Module->ModuleName->IP节点的存储内容,新增[5.5.2 全局配置文件]
+4. 20160606 新增OPENAPI章节与设计文档,拆分各组件README，重新定义任务类型
+5. 20160607 更新概念与分层，定义模式/类型/任务
+6. 20160612 更新分层概念，增加Module结构图，修订Instruct，支持一个模块多个指令，更新数据库结构，变更
---
+++ designer/elves.sql
@@ -0,0 +1,270 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.1.52 : Database - elves_new
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`elves_new` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `elves_new`;
+
+/*Table structure for table `auth_key` */
+
+DROP TABLE IF EXISTS `auth_key`;
+
+CREATE TABLE `auth_key` (
+  `auth_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'authID',
+  `auth_key` varchar(16) NOT NULL COMMENT '秘钥',
+  `key_name` varchar(32) DEFAULT NULL COMMENT '密钥名称',
+  PRIMARY KEY (`auth_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `auth_relation` */
+
+DROP TABLE IF EXISTS `auth_relation`;
+
+CREATE TABLE `auth_relation` (
+  `auth_key` varchar(16) NOT NULL COMMENT '秘钥',
+  `agent_ip` varchar(15) NOT NULL COMMENT 'AgentIP',
+  `module` varchar(32) NOT NULL COMMENT '指令',
+  PRIMARY KEY (`auth_key`,`agent_ip`,`module`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `base_agent` */
+
+DROP TABLE IF EXISTS `base_agent`;
+
+CREATE TABLE `base_agent` (
+  `agent_ip` varchar(15) NOT NULL COMMENT 'AgentIP',
+  `agent_name` varchar(32) DEFAULT NULL COMMENT 'Agent名称',
+  PRIMARY KEY (`agent_ip`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `base_module` */
+
+DROP TABLE IF EXISTS `base_module`;
+
+CREATE TABLE `base_module` (
+  `module` varchar(32) NOT NULL COMMENT '指令',
+  `module_name` varchar(32) DEFAULT NULL COMMENT '模块名',
+  `module_version` varchar(10) DEFAULT NULL COMMENT '模块版本',
+  `charp` tinyint(1) DEFAULT '0' COMMENT 'Csharp支持',
+  `py` tinyint(1) DEFAULT '0' COMMENT 'Csharp支持',
+  PRIMARY KEY (`module`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `log_cron_exec` */
+
+DROP TABLE IF EXISTS `log_cron_exec`;
+
+CREATE TABLE `log_cron_exec` (
+  `code` int(11) NOT NULL AUTO_INCREMENT COMMENT 'code',
+  `log_id` varchar(19) DEFAULT NULL COMMENT '消息ID',
+  `mode` varchar(4) DEFAULT NULL COMMENT '模式',
+  `agent_ip` varchar(15) DEFAULT NULL COMMENT 'AgentIP',
+  `module` varchar(32) DEFAULT NULL COMMENT '模块',
+  `func` varchar(32) DEFAULT NULL COMMENT '方法',
+  `param` text COMMENT '参数',
+  `starttime` datetime DEFAULT NULL COMMENT '开始时间',
+  `agent_sync_flag` tinyint(1) DEFAULT NULL COMMENT '发送agent同步消息状态(1:成功,0:异常)',
+  `agent_sync_error` varchar(255) DEFAULT NULL COMMENT '同步agent消息异常信息',
+  `agent_sync_costtime` int(11) DEFAULT NULL COMMENT '发送agent同步消息耗时',
+  `agent_callback_time` datetime DEFAULT NULL COMMENT 'agent回调时间',
+  `worker_message` text COMMENT 'worker执行结果',
+  `worker_costtime` int(11) DEFAULT NULL COMMENT 'worker执行时间',
+  `module_sync_flag` tinyint(1) DEFAULT NULL COMMENT '发送module同步消息状态(1:成功,0:异常)',
+  `module_sync_error` varchar(255) DEFAULT NULL COMMENT '同步module消息异常信息',
+  `module_sync_costtime` int(11) DEFAULT NULL COMMENT '发送module同步消息耗时',
+  `module_callback_time` datetime DEFAULT NULL COMMENT 'module回调时间',
+  `processor_message` text COMMENT 'processor执行结果',
+  `processor_costtime` int(11) DEFAULT NULL COMMENT 'processor执行时间',
+  `endtime` datetime DEFAULT NULL COMMENT '结束时间',
+  `result_flag` tinyint(1) DEFAULT '2' COMMENT '执行状态(0:失败，1:成功，2:执行中)',
+  PRIMARY KEY (`code`),
+  UNIQUE KEY `log_id` (`log_id`),
+  KEY `client_id` (`agent_ip`,`func`),
+  KEY `starttime` (`starttime`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `log_interface_operate` */
+
+DROP TABLE IF EXISTS `log_interface_operate`;
+
+CREATE TABLE `log_interface_operate` (
+  `code` int(11) NOT NULL AUTO_INCREMENT,
+  `interface_name` varchar(32) DEFAULT NULL COMMENT '接口名称',
+  `auth_key` varchar(16) DEFAULT NULL COMMENT '秘钥',
+  `agent_ip` varchar(15) DEFAULT NULL COMMENT 'AgentIP',
+  `params` text COMMENT '参数',
+  `result` text COMMENT '返回值',
+  PRIMARY KEY (`code`),
+  KEY `interface_name` (`interface_name`),
+  KEY `auth_key` (`auth_key`),
+  KEY `agent_ip` (`agent_ip`)
+) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `log_queue_exec` */
+
+DROP TABLE IF EXISTS `log_queue_exec`;
+
+CREATE TABLE `log_queue_exec` (
+  `code` int(11) NOT NULL AUTO_INCREMENT COMMENT 'code',
+  `log_id` varchar(19) DEFAULT NULL COMMENT '消息ID',
+  `mode` varchar(4) DEFAULT NULL COMMENT '模式',
+  `agent_ip` varchar(15) DEFAULT NULL COMMENT 'AgentIP',
+  `module` varchar(32) DEFAULT NULL COMMENT '模块',
+  `func` varchar(32) DEFAULT NULL COMMENT '方法',
+  `param` text COMMENT '参数',
+  `starttime` datetime DEFAULT NULL COMMENT '开始时间',
+  `agent_sync_flag` tinyint(1) DEFAULT NULL COMMENT '发送agent同步消息状态(1:成功,0:异常)',
+  `agent_sync_error` varchar(255) DEFAULT NULL COMMENT '同步agent消息异常信息',
+  `agent_sync_costtime` int(11) DEFAULT NULL COMMENT '发送agent同步消息耗时',
+  `agent_callback_time` datetime DEFAULT NULL COMMENT 'agent回调时间',
+  `worker_message` text COMMENT 'worker执行结果',
+  `worker_costtime` int(11) DEFAULT NULL COMMENT 'worker执行时间',
+  `module_sync_flag` tinyint(1) DEFAULT NULL COMMENT '发送module同步消息状态(1:成功,0:异常)',
+  `module_sync_error` varchar(255) DEFAULT NULL COMMENT '同步module消息异常信息',
+  `module_sync_costtime` int(11) DEFAULT NULL COMMENT '发送module同步消息耗时',
+  `module_callback_time` datetime DEFAULT NULL COMMENT 'module回调时间',
+  `processor_message` text COMMENT 'processor执行结果',
+  `processor_costtime` int(11) DEFAULT NULL COMMENT 'processor执行时间',
+  `endtime` datetime DEFAULT NULL COMMENT '结束时间',
+  `result_flag` tinyint(1) DEFAULT '2' COMMENT '执行状态(0:失败，1:成功，2:执行中)',
+  PRIMARY KEY (`code`),
+  UNIQUE KEY `log_id` (`log_id`),
+  KEY `client_id` (`agent_ip`,`func`),
+  KEY `starttime` (`starttime`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `log_rt_exec` */
+
+DROP TABLE IF EXISTS `log_rt_exec`;
+
+CREATE TABLE `log_rt_exec` (
+  `code` int(11) NOT NULL AUTO_INCREMENT COMMENT 'code',
+  `log_id` varchar(19) DEFAULT NULL COMMENT '消息ID',
+  `mode` varchar(4) DEFAULT NULL COMMENT '模式',
+  `agent_ip` varchar(15) DEFAULT NULL COMMENT 'AgentIP',
+  `module` varchar(32) DEFAULT NULL COMMENT '模块',
+  `func` varchar(32) DEFAULT NULL COMMENT '方法',
+  `param` text COMMENT '参数',
+  `starttime` datetime DEFAULT NULL COMMENT '开始时间',
+  `agent_sync_flag` tinyint(1) DEFAULT NULL COMMENT '发送agent同步消息状态(1:成功,0:异常)',
+  `agent_sync_error` varchar(255) DEFAULT NULL COMMENT '同步agent消息异常信息',
+  `agent_sync_costtime` int(11) DEFAULT NULL COMMENT '发送agent同步消息耗时',
+  `agent_callback_time` datetime DEFAULT NULL COMMENT 'agent回调时间',
+  `worker_message` text COMMENT 'worker执行结果',
+  `worker_costtime` int(11) DEFAULT NULL COMMENT 'worker执行时间',
+  `module_sync_flag` tinyint(1) DEFAULT NULL COMMENT '发送module同步消息状态(1:成功,0:异常)',
+  `module_sync_error` varchar(255) DEFAULT NULL COMMENT '同步module消息异常信息',
+  `module_sync_costtime` int(11) DEFAULT NULL COMMENT '发送module同步消息耗时',
+  `module_callback_time` datetime DEFAULT NULL COMMENT 'module回调时间',
+  `processor_message` text COMMENT 'processor执行结果',
+  `processor_costtime` int(11) DEFAULT NULL COMMENT 'processor执行时间',
+  `endtime` datetime DEFAULT NULL COMMENT '结束时间',
+  `result_flag` tinyint(1) DEFAULT '2' COMMENT '执行状态(0:失败，1:成功，2:执行中)',
+  PRIMARY KEY (`code`),
+  UNIQUE KEY `log_id` (`log_id`),
+  KEY `client_id` (`agent_ip`,`func`),
+  KEY `starttime` (`starttime`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `task_cron` */
+
+DROP TABLE IF EXISTS `task_cron`;
+
+CREATE TABLE `task_cron` (
+  `code` int(11) NOT NULL AUTO_INCREMENT COMMENT 'code',
+  `id` varchar(19) DEFAULT NULL COMMENT '计划ID',
+  `mode` varchar(4) DEFAULT NULL COMMENT '模式',
+  `agent_ip` varchar(15) DEFAULT NULL COMMENT '客户端IP',
+  `module` varchar(32) DEFAULT NULL COMMENT '模块',
+  `func` varchar(32) DEFAULT NULL COMMENT '方法',
+  `param` text COMMENT '参数',
+  `rule` varchar(155) DEFAULT NULL COMMENT '规则',
+  `flag` tinyint(4) DEFAULT NULL COMMENT '状态(1:正常,0:暂停)',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`code`),
+  UNIQUE KEY `id` (`id`),
+  KEY `client_ip` (`agent_ip`,`func`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `task_queue` */
+
+DROP TABLE IF EXISTS `task_queue`;
+
+CREATE TABLE `task_queue` (
+  `code` int(11) NOT NULL AUTO_INCREMENT COMMENT 'code',
+  `id` varchar(19) DEFAULT NULL COMMENT '队列ID',
+  `agent_ip` varchar(15) DEFAULT NULL COMMENT 'AgentIP',
+  `module` varchar(32) DEFAULT NULL COMMENT '模块',
+  `func` varchar(32) DEFAULT NULL COMMENT '指令',
+  `param` text COMMENT '参数',
+  `depend_tq_id` varchar(16) DEFAULT NULL COMMENT '依赖',
+  `flag` char(1) DEFAULT NULL COMMENT '状态(p:等待,q:已入队,s:已停止)',
+  `call_id` varchar(16) DEFAULT NULL COMMENT '调用ID(自动生成)',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`code`),
+  UNIQUE KEY `tq_id` (`id`),
+  KEY `client_id` (`func`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `task_self` */
+
+DROP TABLE IF EXISTS `task_self`;
+
+CREATE TABLE `task_self` (
+  `code` int(11) NOT NULL AUTO_INCREMENT COMMENT 'code',
+  `ts_id` varchar(19) DEFAULT NULL COMMENT '计划ID',
+  `agent_ip` varchar(15) DEFAULT NULL COMMENT '客户端IP',
+  `instruct` varchar(32) DEFAULT NULL COMMENT '指令',
+  `param` text COMMENT '参数',
+  `rule` varchar(155) DEFAULT NULL COMMENT '规则',
+  `flag` tinyint(4) DEFAULT NULL COMMENT '状态(1:正常,0:暂停)',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`code`),
+  UNIQUE KEY `ts_id` (`ts_id`),
+  KEY `client_ip` (`agent_ip`,`instruct`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/* Procedure structure for procedure `lock_queue` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `lock_queue` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`mysql`@`%` PROCEDURE `lock_queue`(IN T VARCHAR(16) )
+BEGIN
+		CREATE TEMPORARY TABLE locktmp (
+			 `tq_id` VARCHAR(16) DEFAULT NULL COMMENT '队列ID'
+			) ENGINE=MEMORY DEFAULT CHARSET=utf8;
+			INSERT INTO locktmp 
+			SELECT `tq_id` FROM `task_queue` WHERE call_id IS NULL AND (depend_tq_id IS NULL OR depend_tq_id = '')
+					UNION ALL
+					(
+						SELECT c.tq_id
+						FROM(
+						SELECT a.*,b.log_id,b.`result_flag` FROM `task_queue` a
+						LEFT JOIN `log_queue_exec` b 
+						ON a.depend_tq_id = b.log_id
+						WHERE a.call_id IS NULL AND a.depend_tq_id IS NOT NULL) c
+						WHERE  c.result_flag=1 OR c.result_flag=0
+					);
+					
+			UPDATE `task_queue` SET call_id =T WHERE tq_id IN( SELECT * FROM locktmp );
+			DROP TEMPORARY TABLE locktmp;
+    END */$$
+DELIMITER ;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
